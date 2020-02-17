ee51bae0d5dc9e64d8642fb7f756b0c5041d5ce6
Technicallyaustin <Technicallyaustin@gmail.com>
2020-02-16 21:25:44 -0800

    scraper and song class are done, CLI can references other classes. Finish the CLI logic  and you're done.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,16 +2,57 @@
 require_relative './environment'
 require_relative  './scraper'
 require 'pry'
+
 class Billboard100::CLI
 
 def call
 greet_user
 Scraper.get_data
+list_songs
+prompt 
 end
 
 def greet_user
     puts "hello!"
 end
 
+def list_songs
+    Song.all.each do |song|
+         p "#{song.rank}:  #{song.name} "
+    end
+end
+
+def prompt 
+    msg =  "If you want to know more about the song type in the name of the song!"
+    return_msg = "Type 'Top' To see the list again."
+    puts msg
+    input = gets.strip.downcase
+
+    while input != nil do 
+        song_info = Song.all.detect {|song| input == song.name.downcase}
+        if song_info == false 
+            puts "Sorry, there was a problem, can you try again?"
+            prompt 
+
+        elsif song_info  
+        puts " Rank:    #{song_info.rank} "
+        binding.pry
+        puts " Name:       #{song_info.name} "
+        puts " Artist:          #{song_info.artist} "
+        puts " Peak:                #{song_info.peak} "
+        return_msg 
+
+
+    end
+end
+
+
 
 end
+
+
+
+
+end
+
+
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,10 +9,12 @@ def self.get_data
         container = doc.css("article.ye-chart-item")
         limited_array = container[0..9]
         limited_array.each do |song|
-            name = song.css("div.ye-chart-item__title").text
-            artist = song.css("div.ye-chart-item__artist").text
+            name = song.css("div.ye-chart-item__title").text.split("\n").delete_if{|i| i == "" || i == " "}.join
+            artist = song.css("div.ye-chart-item__artist").text.split("\n").delete_if{|i| i == "" || i == " "}.join
             peak = song.css("span.decade-end-chart-item__peak-info-data").text
-            rank = song.css("div.ye-chart-item__rank").text
+            binding.pry
+        
+            rank = song.css("div.ye-chart-item__rank").text.split("\n").delete_if{|i| i == "" || i == " "}.join
             Song.new(rank, name, artist, peak)
         end
 
