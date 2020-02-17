2b62ebd81b34dc6330377ae63cf085d8e7132b14
Technicallyaustin <Technicallyaustin@gmail.com>
2020-02-17 10:57:13 -0800

    Everything works

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,14 +9,17 @@ def call
 greet_user
 Scraper.get_data
 list_songs
-prompt 
+prompt
+goodbye 
 end
 
 def greet_user
     puts "hello!"
+    puts "========== Welcome to the Billboard 100 app! ==========\n"
 end
 
 def list_songs
+                    puts "========= Top Songs ==================="
     Song.all.each do |song|
          p "#{song.rank}:  #{song.name} "
     end
@@ -25,42 +28,36 @@ end
 def prompt 
     msg =  "If you want to know more about the song, type in the name of the song!"
     return_msg = "Type 'Top' To see the list again."
+    exit_msg = "Type Done! to quit the program."
     error = "Sorry, there was a problem, can you try again?"
     puts msg 
+    puts exit_msg 
     input = gets.strip.downcase
     song_info = Song.all.detect  {|song| input == song.name.downcase }
-    binding.pry 
-        unless input == song_info
-          puts error
-          input = gets.strip.downcase
-          song_info
-binding.pry
-        end
-            #binding.pry 
+   # binding.pry 
     #this logic chain works.
-        while input != nil && song_info != nil do 
+    if input.downcase == "Done!".downcase
+        puts "Thanks for stopping by!"
+        exit!
+    elsif song_info
+        puts "============================"
             puts " Rank:    #{song_info.rank} "
             puts " Name:       #{song_info.name} "
             puts " Artist:          #{song_info.artist} "
             puts " Peak:                #{song_info.peak} "
-            return_msg
+            puts "============================"
+            puts return_msg
+            input
+            song_info
             list_songs
-        end
-        input = gets.strip.downcase
+            prompt
+            exit_msg
+    elsif song_info == nil 
+        prompt
+        exit_msg
+    end
 
-    
 
-
-
-
-end
-
-def information
-    return_msg = "Type 'Top' To see the list again."
-        puts " Rank:    #{song_info.rank} "
-        puts " Name:       #{song_info.name} "
-        puts " Artist:          #{song_info.artist} "
-        puts " Peak:                #{song_info.peak} "
 
 
 
